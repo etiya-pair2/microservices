9b01a75b97676c72fb3348068164ce301843a096
SabaUrgup <sabaurgup@gmail.com>
2024-10-16 17:16:37 +0300

    add identity service docker files

--- identityservice/src/main/java/com/etiya/identityservice/service/auth/AuthServiceImpl.java
+++ identityservice/src/main/java/com/etiya/identityservice/service/auth/AuthServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService
